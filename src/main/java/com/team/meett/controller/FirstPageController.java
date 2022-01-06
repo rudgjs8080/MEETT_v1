3e9fc3ce6025903401de3f3f08a1ae191f103c6f
j-hm <thinkalonej@gmail.com>
2022-01-06 15:36:03 +0900

    layout issue

--- src/main/java/com/team/meett/controller/FirstPageController.java
+++ src/main/java/com/team/meett/controller/FirstPageController.java
@@ -1,7 +1,6 @@
 package com.team.meett.controller;
 
 import com.team.meett.dto.UserRequestDto;
-import com.team.meett.model.Users;
 import com.team.meett.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -9,8 +8,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpSession;
 
